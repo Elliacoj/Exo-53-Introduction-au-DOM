6cfe166f472f36910f3a4d1ccf265793b13dd5fb
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-11-24 15:26:18 +0100

    Modif

---
+++ script.js
@@ -0,0 +1,68 @@
+let color = document.getElementById("all");
+color.style.color = "blue";
+color.style.width ="500px"
+
+let spanTest = document.getElementById("spanTest");
+spanTest.style.backgroundColor = "yellow";
+spanTest.dir = 'rtl';
+
+let spanTest2 = document.getElementById("spanTest2");
+spanTest2.style.backgroundColor = "yellow";
+spanTest2.dir = 'rtl';
+
+let spanTest3 = document.getElementById("spanTest3");
+spanTest3.style.backgroundColor = "yellow";
+spanTest3.dir = 'rtl';
+
+let spanTest4 = document.getElementById("spanTest4");
+spanTest4.style.backgroundColor = "yellow";
+spanTest4.dir = 'rtl';
+
+let spanTest5 = document.getElementById("spanTest5");
+spanTest5.style.backgroundColor = "yellow";
+spanTest5.dir = 'rtl';
+
+let spanTest6 = document.getElementById("spanTest6");
+spanTest6.style.backgroundColor = "yellow";
+spanTest6.dir = 'rtl';
+
+let spanTest7 = document.getElementById("spanTest7");
+spanTest7.style.backgroundColor = "yellow";
+spanTest7.dir = 'rtl';
+
+let spanTest8 = document.getElementById("spanTest8");
+spanTest8.style.backgroundColor = "yellow";
+spanTest8.dir = 'rtl';
+
+let spanTest9 = document.getElementById("spanTest9");
+spanTest9.style.backgroundColor = "yellow";
+spanTest9.dir = 'rtl';
+
+let spanTest10 = document.getElementById("spanTest10");
+spanTest10.style.backgroundColor = "yellow";
+spanTest10.dir = 'rtl';
+
+let animation = document.getElementById('animation');
+animation.style.animation = 'texte';
+animation.style.animationDuration = '5s';
+animation.innerText = "Le texte a changé "
+
+let lien1 = document.getElementById('lien1');
+lien1.innerText = "Le nouveau texte";
+lien1.href = "https://www.youtube.com/";
+
+let lien2 = document.getElementById('lien2');
+lien2.innerText = "Le nouveau texte";
+lien2.href = "https://www.youtube.com/";
+
+let lien3 = document.getElementById('lien3');
+lien3.innerText = "Le nouveau texte";
+lien3.href = "https://www.youtube.com/";
+
+let lien4 = document.getElementById('lien4');
+lien4.innerText = "Le nouveau texte";
+lien4.href = "https://www.youtube.com/";
+
+let lien5 = document.getElementById('lien5');
+lien5.innerText = "Le nouveau texte";
+lien5.href = "https://www.youtube.com/";
